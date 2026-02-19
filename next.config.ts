fbb4105a30874cf6ff72188c2a292ec6e8926858
Lener <lenermatos128@gmail.com>
2026-02-19 23:43:04 +0100

    configurando img para vercel

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,11 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
+
+      },
+      {
+        protocol: 'https',
+        hostname: '**.public.blob.vercel-storage.com', // Este es el que permite las imágenes de Vercel Blob
       },
     ],
   },
